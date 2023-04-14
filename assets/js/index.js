002b4d5fb6e10ddd63c90f492e4445be580aab2b
Immad Siddiqui <104530004+immadsidd@users.noreply.github.com>
2023-04-15 02:12:32 +0500

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,238 @@
+// preloader
+var preloader = document.getElementById('loading');
+function preloading() {
+  setTimeout(timeout, 2000);
+}
+function timeout() {
+  preloader.style.display = 'none';
+}
+
+// darkmode
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+// home page reveal
+myID = document.getElementById("head");
+
+var myScrollFunc = function () {
+  var y = window.scrollY;
+  if (y >= 350) {
+    myID.className = "show"
+  } else {
+    myID.className = "hide"
+  }
+};
+
+// intro typewriter
+var typed = new Typed(".multi", {
+  strings: ["Software Engineer", "Frontend Developer", "Illustrator", "Blogger", "Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+
+// skills canvas
+window.addEventListener("scroll", myScrollFunc);
+$(document).ready(function () {
+  if (!$("#myCanvas").tagcanvas({
+    textColour: "#FFFEFF",
+    outlineColour: "transparent",
+    reverse: true,
+    depth: 0.8,
+    initial: [0.3, 0.1],
+    maxSpeed: 0.05,
+    weight: true,
+  }, "tags")) {
+    // something went wrong hide the canvas container,
+    $("#myCanvasContainer");
+  }
+})
+
+// filter work
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+  }
+}
+function AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
+  }
+}
+function RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Link active work 
+const workLinks = document.querySelectorAll(".work__item");
+function activeWork(workLink) {
+  workLinks.forEach((wl) => {
+    wl.classList.remove("active-work");
+  });
+  workLink.classList.add("active-work");
+}
+workLinks.forEach((wl) => {
+  wl.addEventListener("click", () => {
+    activeWork(wl);
+  });
+});
+
+
+// glass effect
+VanillaTilt.init(document.querySelectorAll(".card"), {
+  max: 25,
+  speed: 400,
+  glare: true,
+  "max-glare": 0.5,
+});
+VanillaTilt.init(document.querySelectorAll(".contact__card"), {
+  max: 10,
+  speed: 80,
+  glare: true,
+  "max-glare": 0.5,
+});
+VanillaTilt.init(document.querySelectorAll(".nft"), {
+  max: 10,
+  speed: 80,
+  glare: true,
+  "max-glare": 0.2,
+});
+
+
+// header active link
+const sections = document.querySelectorAll("section[id]");
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+
+// reveal animation
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+
+function reveal1() {
+  var reveals = document.querySelectorAll(".reveal1");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal1);
+
+//SCROLL REVEAL ANIMATION
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  reset: true,
+});
+
+sr.reveal('.home, .multi, .home_button, .illustration, .skills-charts', {
+  delay: 100,
+  origin: "top",
+  scale: 0.9,
+  distance: "30px",
+});
+
+sr.reveal('.about_img', {
+  delay: 100,
+  origin: "left",
+  scale: 0.9,
+  distance: "30px",
+});
+
+sr.reveal(`.about_description, .about__button-contact`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "right",
+  distance: "30px",
+});
+
+sr.reveal(`.skill_description`, {
+  delay: 100,
+  scale: 0.9,
+  origin: "left",
+  distance: "40px",
+});
+
+sr.reveal('.card, .services_description, .work_description', {
+  delay: 100,
+  scale: 0.9,
+  origin: "top",
+  distance: "30px",
+});
+
+// text hover effect
+const element = document.getElementsByClassName("alpha");
+for (let i = 0; i <= element.length; i++) {
+  element[i].addEventListener('animationend', function (e) {
+    element[i].classList.remove('animated');
+  })
+  element[i].addEventListener('mouseover', function (e) {
+    element[i].classList.add('animated');
+  })
+}
+
+const elements = document.getElementsByClassName("skills_alpha");
+for (let i = 0; i <= elements.length; i++) {
+  elements[i].addEventListener('animationend', function (e) {
+    elements[i].classList.remove('animated');
+  })
+  elements[i].addEventListener('mouseover', function (e) {
+    elements[i].classList.add('animated');
+  })
+}
---
+++ assets/js/svg.js
@@ -0,0 +1,144 @@
+var data = {categoryX: 50, points: [{"x":70,"y":161.16},{"x":30,"y":232.58},{"x":50,"y":359.51},{"x":60,"y":360.53},{"x":30,"y":566.63},{"x":30,"y":594.08},{"x":60,"y":650.37},{"x":30,"y":723.46},{"x":60,"y":864.01},{"x":30,"y":879.88},{"x":60,"y":1138.35},{"x":30,"y":1158.8},{"x":30,"y":1250.25},{"x":60,"y":1489.94},{"x":30,"y":1583.51},{"x":30,"y":1627.55},{"x":30,"y":1739.97},{"x":30,"y":1800.63},{"x":30,"y":1885.21},{"x":30,"y":1979.47},{"x":60,"y":5057.81},{"x":150,"y":5059.57},{"x":30,"y":5097.48},{"x":30,"y":2152.41},{"x":30,"y":2152.46},{"x":30,"y":2157.54},{"x":30,"y":2157.56},{"x":30,"y":2174.22},{"x":30,"y":2188.61},{"x":30,"y":2213.54},{"x":30,"y":2231.43},{"x":30,"y":2252},{"x":50,"y":2275.33},{"x":30,"y":2291.35},{"x":70,"y":2319.71},{"x":30,"y":2328.22},{"x":30,"y":2345.74},{"x":25,"y":2345.91},{"x":30,"y":2367.61},{"x":30,"y":2409.25},{"x":30,"y":2429.13},{"x":30,"y":2447.6},{"x":30,"y":2467.67},{"x":30,"y":2473.07},{"x":50,"y":2473.24},{"x":30,"y":2485.47},{"x":30,"y":2528.83},{"x":30,"y":2664.08},{"x":30,"y":2664.16},{"x":25,"y":2665.75},{"x":30,"y":2681.58},{"x":25,"y":2682.98},{"x":25,"y":2683.03},{"x":30,"y":2703.05},{"x":25,"y":2707.75},{"x":30,"y":2719.75},{"x":30,"y":2725.5},{"x":25,"y":2727.59},{"x":30,"y":2741.92},{"x":25,"y":2758.02},{"x":30,"y":2758.88},{"x":30,"y":2766},{"x":25,"y":2768.52},{"x":30,"y":2781.85},{"x":25,"y":2783.5},{"x":30,"y":2790.62},{"x":30,"y":2797.6},{"x":30,"y":2803.05},{"x":30,"y":2822.51},{"x":30,"y":2842.45},{"x":30,"y":2859.41},{"x":25,"y":2861.83},{"x":25,"y":2861.9},{"x":30,"y":2878.11},{"x":30,"y":2899.3},{"x":30,"y":2899.39},{"x":150,"y":2900.16},{"x":30,"y":2915.11},{"x":30,"y":2954.28},{"x":30,"y":2963.22},{"x":30,"y":2993.16},{"x":30,"y":3024.93},{"x":30,"y":3034.73},{"x":30,"y":3053.71},{"x":100,"y":3108.65},{"x":30,"y":3108.78},{"x":100,"y":3170.02},{"x":30,"y":3307.02},{"x":30,"y":3347},{"x":100,"y":3386.84},{"x":30,"y":3388.35},{"x":30,"y":3392.8},{"x":30,"y":3409.08},{"x":50,"y":3482.29},{"x":30,"y":3490.06},{"x":100,"y":3490.08},{"x":30,"y":3521.3},{"x":30,"y":3525.8},{"x":30,"y":3527.47},{"x":100,"y":3527.71},{"x":350,"y":3785.85},{"x":30,"y":3817.04},{"x":100,"y":3879.62},{"x":30,"y":3990.06},{"x":100,"y":3994.39},{"x":30,"y":4033.28},{"x":150,"y":4040},{"x":30,"y":4168.39},{"x":30,"y":4503.66},{"x":30,"y":4309.47},{"x":30,"y":4355.49},{"x":30,"y":4438.63},{"x":30,"y":4501.4},{"x":30,"y":4527.18},{"x":70,"y":4527.24},{"x":30,"y":4527.32},{"x":30,"y":4705.31},{"x":50,"y":4790.07},{"x":30,"y":4790.17},{"x":30,"y":4831.58},{"x":30,"y":4909.79},{"x":30,"y":4978.06},{"x":30,"y":5024.68},{"x":30,"y":5341.95},{"x":30,"y":5404.04},{"x":30,"y":5707.09},{"x":30,"y":5846.84},{"x":30,"y":5918.93},{"x":30,"y":5956.66},{"x":30,"y":6008.86},{"x":30,"y":6032.41},{"x":30,"y":6074.86},{"x":30,"y":6160.47},{"x":30,"y":6494.77},{"x":30,"y":6544.9},{"x":30,"y":6554.59},{"x":30,"y":6572.89},{"x":30,"y":6650.47},{"x":30,"y":6628.18},{"x":30,"y":6651.49},{"x":30,"y":6690.64},{"x":30,"y":6734.86},{"x":30,"y":6784.42},{"x":30,"y":6897.47},{"x":50,"y":6978.9},{"x":30,"y":7053.46},{"x":30,"y":7064.33},{"x":30,"y":7081.25},{"x":50,"y":7169.69},{"x":30,"y":7169.81},{"x":30,"y":7509.83},{"x":30,"y":7258.2},{"x":100,"y":7352.41},{"x":30,"y":7365.26},{"x":70,"y":7437.33},{"x":30,"y":7556.63},{"x":30,"y":7589.71},{"x":30,"y":7626.93},{"x":30,"y":7649.84},{"x":30,"y":7666.02},{"x":30,"y":7689.28},{"x":30,"y":7705.31},{"x":30,"y":7727.3},{"x":30,"y":7744.38},{"x":30,"y":7766.83},{"x":30,"y":7817.81},{"x":30,"y":7862.84},{"x":400,"y":7882.63},{"x":30,"y":8036.55},{"x":30,"y":8192.58},{"x":30,"y":8419.56},{"x":30,"y":8566.87},{"x":30,"y":8667.14},{"x":30,"y":8780.15},{"x":30,"y":8784.1},{"x":30,"y":8827.76},{"x":30,"y":8855.53},{"x":30,"y":8890.78},{"x":30,"y":8939.6},{"x":30,"y":8997.51},{"x":30,"y":9169.01},{"x":30,"y":9314.37},{"x":30,"y":9399.17},{"x":30,"y":9594.91},{"x":500,"y":9756.56},{"x":50,"y":10109.02}]};
+
+var svg = d3.select('svg');
+var distancePath = svg.append('path')
+  .attr('fill', 'none')
+  .attr('stroke', 'none').node();
+
+var source;
+var target;
+var gap = 100;
+var totalDistance = 0;
+_.each(data.points, function(target) {
+  if (!source) {
+    target.d = 'M' + target.x + ',' + target.y;
+  } else {
+    if (source.y > target.y - gap) {
+      // if they're sufficiently close to each other
+      if (source.x === target.x) {
+        target.d = drawLine(target.x, target.y);
+        setDistance(source, target);
+      } else {
+        target.d = drawCurve(source.x, source.y, target.x, target.y);
+
+        setDistance(source, target);
+        target.y1 = target.y - source.y;
+        target.interpolate1 = d3.interpolate(0, target.distance);
+      }
+    } else {
+      target.d = '';
+      if (source.x !== data.categoryX) {
+        // if the source repo owner isn't the same as the contributor, move the line back
+        target.d += drawCurve(source.x, source.y, data.categoryX, source.y + gap / 3);
+
+        setDistance(source, target);
+        target.y1 = gap / 3;
+        target.interpolate1 = d3.interpolate(0, target.distance);
+      }
+
+      x = target.x;
+      y = target.y;
+      if (target.x !== data.categoryX) {
+        x = data.categoryX;
+        y = target.y - gap / 3;
+      }
+      
+      target.d += drawLine(x, y);
+      target.y2 = y - (target.y1 || 0);
+      setDistance(source, target);
+
+      if (target.x !== data.categoryX) {
+        target.d += drawCurve(x, y, target.x, target.y);
+        var currentDistance = target.distance;
+        setDistance(source, target);
+        target.y3 = gap / 3;
+        target.interpolate2 = d3.interpolate(0, target.distance - currentDistance);
+      }
+    }
+    target.totalDistance = (totalDistance += target.distance);
+    
+  }
+  source = target;
+});
+
+var path = svg.append('path')
+  .attr('d', _.pluck(data.points, 'd').join(' '))
+  .attr('fill-opacity', 0)
+  .attr('stroke', '#FFFFFF')
+  .attr('stroke-width', 4)
+  .attr('stroke-linecap', 'round')
+  .attr('stroke-dasharray', totalDistance)
+  .attr('stroke-dashoffset', totalDistance);
+
+window.addEventListener('scroll', _.throttle(windowScroll, 10));
+
+function windowScroll() {
+  var top = scrollY + 230;
+  var source;
+  var target = _.find(data.points, function(point) {
+    if (point.y >= top) {
+      return true;
+    }
+    source = point;
+    return false;
+  });
+  if (source && target) {
+    var distance = 0;
+    var distanceFromSource = top - source.y;
+    if (!target.interpolate1) {
+      // if there's no interpolate1
+      if (!target.interpolate2) {
+        // and there's no interpolate2, must mean it's a straight line
+        distance = distanceFromSource + source.totalDistance;
+      } else {
+        // if there's a interpolate2, must mean there's a straight line
+        // and then a curve at the end, so figure out if we're in straight line or curve part
+        if (distanceFromSource <= target.y2) {
+          // it's in straight line part
+          distance = distanceFromSource + source.totalDistance;
+        } else {
+          // if it's in last curve part, first interpolate the curve
+          // and then add that back to the straight part and the previous total distance
+          var partialDistance = (distanceFromSource - target.y2) / target.y3;
+          distance = target.interpolate2(partialDistance) + target.y2 + source.totalDistance;
+        }
+      }
+    } else {
+      // if there's interpolate1, must mean there's a first curve
+      if (distanceFromSource <= target.y1) {
+        // so if it's within the first curve, interpolate that and add it to total distance
+        var partialDistance = distanceFromSource / target.y1;
+        distance = target.interpolate1(partialDistance) + source.totalDistance;
+      } else if (distanceFromSource <= (target.y1 + target.y2)) {
+        // if we're in line part, add curve to it
+        distance = target.interpolate1(1) + (distanceFromSource - target.y1) + source.totalDistance;
+      } else if (interpolate2) {
+        var partialDistance = (distanceFromSource - target.y2 - target.y1) / target.y3;
+        distance = target.interpolate1(1) + target.y2 + target.interpolate2(partialDistance);
+      }
+    }
+    // var partialDistance = (top - source.y) / (target.y - source.y);
+    // var distance = target.interpolater(partialDistance) + source.totalDistance;
+    path.transition().duration(10)
+      .attr('stroke-dashoffset', totalDistance - (distance || 0));
+  }
+  
+};
+
+function drawLine(x, y) {
+  return 'L' + x + ',' + y;
+}
+
+function drawCurve(x1, y1, x2, y2) {
+  var cy = (y1 + y2) / 2;
+  return 'C' + x1 + ',' + cy + ' ' + x2 + ',' + cy + ' ' + x2 + ',' + y2;
+}
+
+function setDistance(source, target) {
+  var distancePathD = 'M' + source.x + ',' + source.y + ' ' + target.d;
+  distancePath.setAttribute('d', distancePathD);
+  target.distance = parseFloat(distancePath.getTotalLength().toFixed(2));
+}
+
+
+
